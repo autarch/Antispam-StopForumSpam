729cbc3cdeacbd8668988adb4eab13c450da8da7
Dave Rolsky <autarch@urth.org>
2011-01-16 13:07:54 -0600

    Docs for BerkeleyDB class

--- lib/Antispam/StopForumSpam/BerkeleyDB.pm
+++ lib/Antispam/StopForumSpam/BerkeleyDB.pm
@@ -41,3 +41,42 @@ around build => sub {
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+# ABSTRACT: Manage Berkeley DB files based on StopForumSpam data
+
+__END__
+
+=head1 SYNOPSIS
+
+  Antispam::StopForumSpam::BerkeleyDB->build(
+      file     => '/tmp/listed_email_7.zip',
+      database => '/var/lib/sfs/listed_email_7/listed_email_7.db',
+  );
+
+  my $db = Antispam::StopForumSpam::BerkeleyDB->new(
+      database => '/var/lib/sfs/listed_email_7/listed_email_7.db',
+  );
+
+=head1 DESCRIPTION
+
+This class provides a concrete implementation of the
+L<Antispam::Toolkit::Role::BerkeleyDB> role.
+
+=head1 METHODS
+
+This class consumes all the methods (and attributes) of
+L<Antispam::Toolkit::Role::BerkeleyDB> as-is, except for the C<<
+$class->build() >> method.
+
+The build method will accept a zip file downloaded from the
+L<http://stopforumspam.com> website. You do not need to unzip it first.
+
+=head1 ROLES
+
+This class does the L<Antispam::Toolkit::Role::BerkeleyDB> role.
+
+=head1 BUGS
+
+See L<Antispam::StopForumSpam> for bug reporting details.
+
+=cut
